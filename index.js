1033e69000b22b6c8fb8fab0e87cd7a1e48b690f
Shiyam Sarker <78273465+shiyam-sarker10@users.noreply.github.com>
2024-04-02 15:14:23 +0600

    added some upgrades

--- index.js
+++ index.js
@@ -1,25 +1,16 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
-const {
-  MongoClient,
-  ServerApiVersion,
-  ObjectId,
-  ConnectionPoolClosedEvent,
-} = require("mongodb");
-
 const app = express();
 const port = process.env.PORT || 5000;
 
-//! middleware
-app.use(cors());
 app.use(express.json());
+app.use(cors());
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.obcasl9.mongodb.net/?retryWrites=true&w=majority`;
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASS);
 
-
-//! connect with mongoDB
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u5hejig.mongodb.net/?retryWrites=true&w=majority`;
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -29,29 +20,49 @@ const client = new MongoClient(uri, {
 });
 async function run() {
   try {
-    // Connect the client to the server    (optional starting in v4.7)
-    // await client.connect();
-    //! all collections
-    const userCollection = client.db("devlab").collection("userInfo");
+    const taskCollection = client.db("TaskieeDB").collection("tasks");
+    const bookCollection = client.db("TaskieeDB").collection("books");
 
-    //! Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    app.post("/addBook", async (req, res) => {
+      const book = req.body;
+      const result = await bookCollection.insertOne(book);
+      res.send(result);
+    });
+   
+
+    app.get("/books", async (req, res) => {
+      try {
+        const books = await bookCollection.find({}).toArray();
+        res.json(books);
+      } catch (error) {
+        console.error("Error fetching users:", error);
+        res.status(500).json({ error: "Internal server error" });
+      }
+    });
+
+
+    app.delete("/deleteTask", async (req, res) => {
+      try {
+        if (!req.query.id) {
+          return res.status(400).json({ error: "No ID Provided" });
+        }
+        const id = req.query.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await taskCollection.deleteOne(query);
+        res.json(result);
+      } catch (error) {
+        console.error("Error updating request:", error);
+        res.status(500).json({ error: "Internal sever error" });
+      }
+    });
   } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
   }
 }
 run().catch(console.dir);
 
-//! initially run the server
 app.get("/", (req, res) => {
-  res.send("Devlab server is running");
+  res.send("the project is working");
 });
 app.listen(port, () => {
-  console.log(`Server is running on PORT ${port}`);
+  console.log(`This is server is running on port : ${port}`);
 });
-
-// "dev": "nodemon src/app.js",
